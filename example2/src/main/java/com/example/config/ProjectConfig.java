3616bba1ccc2f3d81af20dbb8021db5e29c46e0b
hintran_05 <you@example.com>
2026-01-01 21:56:36 +0700

    woking with @primary annotation

--- example2/src/main/java/com/example/config/ProjectConfig.java
+++ example2/src/main/java/com/example/config/ProjectConfig.java
@@ -3,12 +3,14 @@ package com.example.config;
 import com.example.beans.Vehicle;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 
 @Configuration
 public class ProjectConfig {
 
     @Bean("ToyotaVehicle")
+    @Primary
     Vehicle vehicle1() {
         Vehicle veh = new Vehicle();
         veh.setName("Toyota");
