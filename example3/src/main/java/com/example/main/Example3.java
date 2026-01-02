2ddda8972b0745a382e0d8f3f89829184374c052
hintran_05 <you@example.com>
2026-01-02 22:31:52 +0700

    understanding @PostConstruct Annotation

---
+++ example3/src/main/java/com/example/main/Example3.java
@@ -0,0 +1,18 @@
+package com.example.main;
+
+import com.example.beans.Vehicle;
+import com.example.config.ProjectConfig;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Example3 {
+
+    public static void main(String[] args) {
+        // Khoi tao thong qua @Bean annotation
+        var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
+        Vehicle veh = context.getBean(Vehicle.class);
+
+        System.out.println("Vehicle name from non-spring context is " + veh.getName());
+        veh.printHello();
+
+    }
+}
