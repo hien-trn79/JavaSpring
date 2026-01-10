04774029c686f9aa4c57042a2253884183adff6b
hintran_05 <you@example.com>
2026-01-10 22:05:34 +0700

    wiring beans using method parameters

---
+++ example9/src/main/java/com/example/config/ProjectConfig.java
@@ -0,0 +1,28 @@
+package com.example.config;
+
+import com.example.beans.Person;
+import com.example.beans.Vehicle;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+@Configuration
+public class ProjectConfig {
+
+    @Bean
+    public Vehicle vehicle() {
+        Vehicle vehicle = new Vehicle();
+        vehicle.setName("Toyota");
+        return vehicle;
+    }
+
+    @Bean
+    public Person person(Vehicle vehicle) {
+        Person person = new Person();
+        person.setName("lucy");
+        // tao mot ket noi dependencies cua person den Vehicle class
+        person.setVehicle(vehicle);
+        return person;
+    }
+}
